b70468671d0e3615e10f979001b2a0b7c4995147
tuyishime jeandamour <tuyishimejeand88@gmail.com>
2024-07-06 09:33:39 +0200

    Update 1-top_ten.py to print none

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,24 +1,20 @@
-#!/usr/bin/python3
-"""Script that fetch 10 hot post for a given subreddit."""
 import requests
 
-
 def top_ten(subreddit):
-    """Return number of subscribers if @subreddit is valid subreddit.
-    if not return 0."""
-
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(subreddit_url, headers=headers)
-
-    if response.status_code == 200:
-        json_data = response.json()
-        for i in range(10):
-            print(
-                json_data.get('data')
-                .get('children')[i]
-                .get('data')
-                .get('title')
-            )
-    else:
+    headers = {'User-Agent': 'MyBot/1.0'}
+    
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        
+        if response.status_code == 200:
+            data = response.json()
+            
+            posts = data['data']['children']
+            for post in posts:
+                print(post['data']['title'])
+        else:
+            print(None)
+    except:
         print(None)
