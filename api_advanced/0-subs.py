f16b90e86f1c1d8372503eb3ee42a1feeddbda7c
tuyishime jeandamour <tuyishimejeand88@gmail.com>
2024-07-06 09:38:22 +0200

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
+"""Script that fetch number of subscriber for a given subreddit."""
 import requests
 
+
 def number_of_subscribers(subreddit):
-    headers = {'User-Agent': 'MyBot/1.0'}
-    
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except:
+    """Return number of subscribers is @subreddit is valid subreddit.
+    if not return 0."""
+
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
+    response = requests.get(subreddit_url, headers=headers)
+
+    if response.status_code == 200:
+        json_data = response.json()
+        subscriber_number = json_data.get('data').get(
+            'children')[0].get('data').get('subreddit_subscribers')
+        return subscriber_number
+    else:
         return 0
